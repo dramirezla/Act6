fe760a5aebeed3751110a5663ad1e09dfa9047aa
dramirezla <53583408+dramirezla@users.noreply.github.com>
2023-06-22 15:00:48 -0500

    Add files via upload

---
+++ Act6Individual/src/main/java/com/mycompany/act6individual/Act6Individual.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.act6individual;
+
+/**
+ *
+ * @author David R.L
+ */
+public class Act6Individual {
+
+    public static void main(String[] args) {
+        InterfazUsuario Formulario= new InterfazUsuario();
+        Formulario.setVisible(true);        
+    }
+}
